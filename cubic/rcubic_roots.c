30f61d2cf7a34ca124fc0623ac71a1ddc86dd7c1
Joey <ync12@ic.ac.uk>
2015-02-12 00:18:20 +0000

    Change method to determine termination of whileloop

--- cubic/rcubic_roots.c
+++ cubic/rcubic_roots.c
@@ -8,14 +8,17 @@ int rcubic_roots(double a2, double a1, double a0, double* r1, double* r2, double
   a = -cbrt(2*a2*a2*a2-9*a1*a2+27*a0)/3.;
   b = -a2/3.;
   p = (a1 - a2*a2/3.)/(a*a);
+  //printf("%10.5g, %10.5g, %10.5g\n", a, b, p);
 
   double y_n, y_n1;
-  y_n  = fabs(p) < 2 ? 1-p/3. : 1/p;
+  y_n  = fabs(p) < 2. ? 1-p/3. : 1/p;
   y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
-  while (y_n != y_n1)
+  //printf("%10.5g, %10.5g\n", y_n, y_n1);
+  while (fabs(y_n-y_n1)>1e-15)
   {
     y_n = y_n1;
     y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
+    printf("%10.5g, %10.5g\n", a*y_n+b, a*y_n1+b);
   }
   *r1 = a*y_n1+b;
 
--- cubic/test.c
+++ cubic/test.c
@@ -9,12 +9,13 @@ void test(int, double, double, double, double, double, double);
 int main()
 {
   test(1,  4.,  -8.,   7., -5.6389, 0.81944, 0.75492);
+  test(2,  3.,   1.,   4., -3.0946, 0.04728, 1.13594);
 }
 
 void test(int index, double a2, double a1, double a0, double r1, double r2, double r3)
 {
   double _r1, _r2, _r3;
-  int _n = rcubic_roots(4., -8., 7., &_r1, &_r2, &_r3);
+  int _n = rcubic_roots(a2, a1, a0, &_r1, &_r2, &_r3);
   if (assertEqual(_r1, r1) &&
       assertEqual(_r2, r2) &&
       assertEqual(_r3, r3))
