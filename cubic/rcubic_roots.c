2bce653d02d2d2ace23135e42dfc3ae875a13039
Joey Chan <yam.chan12@imperial.ac.uk>
2015-02-12 17:00:10 +0000

    Implemented 3ii

--- cubic/rcubic_roots.c
+++ cubic/rcubic_roots.c
@@ -21,33 +21,42 @@ int rcubic_roots(double a2, double a1, double a0, double* r1, double* r2, double
     return ONE_ROOT;
   }
 
-
-  a = -cbrt(2*a2*a2*a2-9*a1*a2+27*a0)/3.;
-  if (a == 0)
-  /* alpha is 0, p cannot be calculated */
+  else if (a0 == 0)
   {
     *r1 = 0;
-    result = quad_roots(1., 0., (a1 - a2*a2/3.), r2, r3);
+    result = quad_roots(1., a2, a1, r2, r3);
   }
-  else
-  /* Normal calculation */
-  {
-    b = -a2/3.;
-    p = (a1 - a2*a2/3.)/(a*a);
-    //printf("%10.5g, %10.5g, %10.5g\n", a, b, p);
 
-    y_n  = fabs(p) < 2. ? 1-p/3. : 1/p;
-    y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
-    //printf("%10.5g, %10.5g\n", y_n, y_n1);
-    while (fabs(y_n-y_n1)>1e-15)
+  else
+  {
+    a = -cbrt(2*a2*a2*a2-9*a1*a2+27*a0)/3.;
+    if (a == 0)
+    /* alpha is 0, p cannot be calculated */
     {
-      y_n = y_n1;
-      y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
-      //printf("%10.5g, %10.5g\n", a*y_n+b, a*y_n1+b);
+      *r1 = 0;
+      result = quad_roots(1., 0., (a1 - a2*a2/3.), r2, r3);
     }
-    *r1 = a*y_n1+b;
-    result = quad_roots(1., a2+(*r1), -a0/(*r1), r2, r3) + 1;
+    else
+    /* Normal calculation */
+    {
+      b = -a2/3.;
+      p = (a1 - a2*a2/3.)/(a*a);
+      //printf("%10.5g, %10.5g, %10.5g\n", a, b, p);
+
+      y_n  = fabs(p) < 2. ? 1-p/3. : 1/p;
+      y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
+      //printf("%10.5g, %10.5g\n", y_n, y_n1);
+      while (fabs(y_n-y_n1)>1e-15)
+      {
+        y_n = y_n1;
+        y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
+        //printf("%10.5g, %10.5g\n", a*y_n+b, a*y_n1+b);
+      }
+      *r1 = a*y_n1+b;
+      result = quad_roots(1., a2+(*r1), -a0/(*r1), r2, r3) + 1;
+    }
   }
+
 
   /* Compute the status which should be returned to main function */
   if (result != COMPLEX_ROOT)
