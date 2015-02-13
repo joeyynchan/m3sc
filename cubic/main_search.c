43dfa462651ba62d100395271cbb79a23751c189
Joey Chan <yam.chan12@imperial.ac.uk>
2015-02-13 18:34:29 +0000

    Added main_search.c

--- cubic/main_search.c
+++ cubic/main_search.c
@@ -7,16 +7,9 @@ int main()
 	double T;
 	double r1, r2, r3;
 	//mprintf("\n%5s %10s %10s %10s %10s %10s\n", "T", "Fst", "Re(Snd)", "Im(Snd)", "Re(Trd)", "Im(Trd)");
-	for (T = 0.; T <= 75000.; T += 1000.)
+	for (T = 0.; T <= 75000.; T += 1.)
 	{
 		int result = rcubic_roots(14., T/2. - 12456., 5.*T - 24960., &r1, &r2, &r3);
-		
-		if (result > 0)
-			printf("%5g %10.5f %10.5f %10.5f %10.5f %10.5f\n", T, r1, r2, 0., r3, 0.);
-		else
-			printf("%5g %10.5f %10.5f %10.5f %10.5f %10.5f\n", T, r1, r2, -fabs(r3), r2, fabs(r3));
-		
-
 
 		if (result > 0 && (r1 == 0 || r2 == 0 || r3 == 0))
 			printf("When T = %5g , one of the 3 real roots takes the value 0\n", T);
