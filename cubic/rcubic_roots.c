f23e897c92f700a693e04edd7864899be9178509
Joey Chan <yam.chan12@imperial.ac.uk>
2015-02-13 15:05:26 +0000

    Implementing vi

--- cubic/rcubic_roots.c
+++ cubic/rcubic_roots.c
@@ -8,30 +8,52 @@ void swap(double*, double*);
 int rcubic_roots(double a2, double a1, double a0, double* r1, double* r2, double* r3)
 {
 
+  /* ALLOWED_ERROR is use to determine if two doubles are the "same"
+   * If two double, a and b, are the "same", then fabs(a-b) < ALLOWED_ERROR
+   * I choose to use a customizable standard so later if I want to improve the acurracy,
+   * I only have to change the ALLOWED_ERROR parameter
+   */
+  double ALLOWED_ERROR = DBL_EPSILON;
+
+  /* For result clarity */
   int THREE_DISTINCT_ROOTS = 3;
   int TWO_REPEATING_ROOTS = 2;
   int ONE_ROOT = 1;
   int COMPLEX_ROOT = 0;
 
-	double ALLOWED_ERROR = 1e-15;
+  double UNITY_1  = 1.;           /* Unity Real root */
+  double UNITY_RE = -1./2.;       /* Real part of the complex unity root */
+  double UNITY_IM = sqrt(3.)/2.;  /* Imaginary part of the complex unity root */
 
   int result;
   double a, b, p;
   double y_n, y_n1;
-
   
-  if (a2 == 0 && a1 == 0)
+  /* Case i: a2 == a1 == 0
+   * Roots are unity times -sign(a0)|a0)^(1/3)
+   */
+  if (fabs(a2) < ALLOWED_ERROR &&
+      fabs(a1) < ALLOWED_ERROR)
   {
-    *r1 = *r2 = *r3 = cbrt(-a0);
-    return ONE_ROOT;
+    *r1 = *r2 = *r3 = (a0 < 0) ? cbrt(-a0) : -cbrt(a0);
+    *r2 *= UNITY_RE;
+    *r3 *= UNITY_IM;
+    return COMPLEX_ROOT;
   }
 
-  else if (a0 == 0)
+  /* Case ii: a0 == 0
+   * One root = 0
+   * Call quad_roots() to find the other two roots
+   */
+  else if (fabs(a0) < ALLOWED_ERROR)
   {
     *r1 = 0;
     result = quad_roots(1., a2, a1, r2, r3);
   }
 
+  /* Case iii: a0 = a1*a2
+   * x = +-(-a1)^(1/2), -a2
+   */
 	else if (fabs(a0 - a1*a2) < ALLOWED_ERROR)
 	{
 		*r1 = -a2;
@@ -48,34 +70,46 @@ int rcubic_roots(double a2, double a1, double a0, double* r1, double* r2, double
 		result = (a1 > 0) ? COMPLEX_ROOT : THREE_DISTINCT_ROOTS ;
 	}
 
-  else if (3*a1 == a2*a2 && 27*a0 == a2*a2*a2)
+  /* Case iv: 3*a1 == (a2)^2 && 27*a0 = (a2)^3
+   * # identical roots: -(a2)/3
+   */
+  else if ( fabs(3*a1 - a2*a2) < ALLOWED_ERROR && 
+            fabs(27*a0 - a2*a2*a2) < ALLOWED_ERROR)
   {
     *r1 = *r2 = *r3 = -a2/3.;
   }
 
+
   else
   {
     a = -cbrt(2*a2*a2*a2-9*a1*a2+27*a0)/3.;
-    if (a == 0)
+
     /* alpha is 0, p cannot be calculated */
+    if (fabs(a) < ALLOWED_ERROR)
     {
       *r1 = 0;
       result = quad_roots(1., 0., (a1 - a2*a2/3.), r2, r3);
     }
-    else
+
+
     /* Normal calculation */
+    else
     {
       b = -a2/3.;
       p = (a1 - a2*a2/3.)/(a*a);
       //printf("%10.5g, %10.5g, %10.5g\n", a, b, p);
 
-      if (p == 0)
+      /* Case v: p = 0
+       * y is the three cubic roots
+       */
+      if (fabs(p) < ALLOWED_ERROR)
       {
-        *r1 = 1.;
-        *r2 = -1./2.;
-        *r3 = sqrt(3.)/2.;
+        *r1 = UNITY_1  * a + b;
+        *r2 = UNITY_RE * a + b;
+        *r3 = UNITY_IM * a + b;
         result = COMPLEX_ROOT;
       }
+
       else
       {
 
@@ -94,8 +128,9 @@ int rcubic_roots(double a2, double a1, double a0, double* r1, double* r2, double
         while ( (fabs(y_n-y_n1) > ALLOWED_ERROR || difference > fabs(y_n1-y_n)) && 
                 count < 3)
         {
+          /* Update difference and counter before next iteration */
           count++;
-          difference = fabs(y_n1-y_n); /* Update difference before next iteration */
+          difference = fabs(y_n1-y_n); 
 
           y_n = y_n1;
           y_n1 = y_n - (y_n*y_n*y_n + p*y_n - 1)/(3*y_n*y_n + p);
